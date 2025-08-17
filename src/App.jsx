9ef8c1e29105563db4344dfdb06f16c671279fe7
Darshan-Paliwal <darshanpaliwal.work@gmail.com>
2025-08-17 21:31:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-// src/App.jsx
+import Hero from "./components/Hero";
+
 export default function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold tracking-wide">
-        🚀 Hello World, Tailwind + React + Vite is Working!
-      </h1>
+    <div className="font-sans">
+      <Hero />
     </div>
-  )
+  );
 }
