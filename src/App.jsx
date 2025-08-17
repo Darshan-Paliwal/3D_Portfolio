b0c174a417c5aaa79789ce0fcf27cfd8f3e1f276
Darshan-Paliwal <darshanpaliwal.work@gmail.com>
2025-08-17 23:02:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 
 export default function App() {
   return (
-    <div className="font-sans">
+    <div className="min-h-screen">
       <Hero />
     </div>
   );
