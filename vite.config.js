1999210b5455e132f79ab59fd3c03ddb934aba61
Darshan-Paliwal <darshanpaliwal.work@gmail.com>
2025-08-18 15:16:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // project root
-  build: {
-    outDir: 'dist', // Vercel expects 'dist' for Vite
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // optional: allows import like "@/components/Hero.jsx"
-    },
-  },
-});
+})
